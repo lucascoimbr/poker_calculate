e6828a005699d4a1e0e4bbad20d49222ff43b4f0
Lucas Coimbra <lucas.a.coimbra@gmail.com>
2022-04-27 11:47:38 -0300

    cards creators

---
+++ holdem.py
@@ -0,0 +1,3 @@
+hand_rankings = ["High Card", "Pair", "Two Pair", "Three of a Kind",
+                 "Straight", "Flush", "Full House", "Four of a Kind",
+                 "Straight Flush", "Royal Flush"]
